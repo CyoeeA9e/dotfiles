8214da90eb891a093ab131ef8d4bd9a3c3e25ede
CyoeeA9e <CyoeeA9e@gmail.com>
2024-11-15 20:19:19 +0800

    zellij

--- fish/config.fish
+++ fish/config.fish
@@ -3,10 +3,18 @@ if status is-interactive
 end
 
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+  set tmp (mktemp -t "yazi-cwd.XXXXXX")
+  yazi $argv --cwd-file="$tmp"
+  if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+    builtin cd -- "$cwd"
+  end
+  rm -f -- "$tmp"
 end
+
+if set -q ZELLIJ
+else
+  if set -q TERM
+    zellij
+  end
+end
+
